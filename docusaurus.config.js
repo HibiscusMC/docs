bdb68a4c572a4683129839dbcd49837545efd99a
LoJoSho <37521985+LoJoSho@users.noreply.github.com>
2023-11-16 12:56:01 -0600

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -71,7 +71,7 @@ const config = {
             items: [
               {
                 label: 'Docs',
-                to: 'docs/intro',
+                to: '',
               },
             ],
           },

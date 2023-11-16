a6c573a7dcec5f0998a0b20271e571692e352104
LoJoSho <37521985+LoJoSho@users.noreply.github.com>
2023-11-16 13:00:40 -0600

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -71,7 +71,7 @@ const config = {
             items: [
               {
                 label: 'Docs',
-                to: '',
+                href: 'https://docs.hibiscusmc.com/',
               },
             ],
           },
